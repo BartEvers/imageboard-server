eab342646c02aed04949705b93ace803512c55e9
Bart Evers <bart@gillz.nl>
2020-06-06 18:03:48 +0200

    initial commit

---
+++ image/router.js
@@ -0,0 +1,20 @@
+const { Router } = require('express')
+const Image = require('./model')
+const auth = require('../auth/middleware')
+
+const router = new Router()
+router.get('/image', (request, response, next) => {
+  Image
+    .findAll()
+    .then(images => response.send(images))
+    .catch(next)
+})
+
+router.post('/image', auth, (request, response, next) => {
+  Image
+    .create(request.body)
+    .then(image => response.send(image))
+    .catch(next)
+})
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const imageRouter = require('./image/router')
+const authRouter = require('./auth/router')
+const userRouter = require('./user/router')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+app.use(imageRouter)
+app.use(authRouter)
+app.use(userRouter)
+
+const port = process.env.PORT || 4001
+app.listen(
+  port,
+  () => console.log(`Listening on :${port}`)
+)
